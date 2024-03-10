eeb3d3668d143d39915b1d5d2b7ee2d857b7ab80
unknown <dampudia@unipacifico.edu.co>
2024-03-09 21:13:13 -0500

    se solucionó el error del contador. código completado

--- taller_FIFO_pacientes/Pacientes.cpp
+++ taller_FIFO_pacientes/Pacientes.cpp
@@ -13,35 +13,36 @@ struct Pacientes
 {
     int acumulador;
     int Cedula = 0;
-    char Nombre [20]; // se pone un limitador de letras que la variable Nombre puede guardar
-    char Apellido [20]; // se pone un limitador de letras que la variable Apellido puede guardar
+    char Nombre[20];   // se pone un limitador de letras que la variable Nombre puede guardar
+    char Apellido[20]; // se pone un limitador de letras que la variable Apellido puede guardar
     Pacientes *sig;
 };
 Pacientes *cab, *aux, *aux2;
-void registrar()
+int registrar()
 {
     if (cab == NULL)
     {
         cab = (struct Pacientes *)malloc(sizeof(struct Pacientes));
-        cout << "Ingrese su cédula: ";
+        cout << "Ingrese su cedula: ";
         cin >> cab->Cedula;
 
         cout << "ingrese su primer nombre: ";
-        cin>> cab-> Nombre;
+        cin >> cab->Nombre;
 
-        cout<<"ingrese su primer apellido: ";
-        cin>> cab -> Apellido;
+        cout << "ingrese su primer apellido: ";
+        cin >> cab->Apellido;
 
         cab->sig = NULL;
 
-        if(cab==NULL){
-            cab=aux;
-            cab->acumulador=1;
-            aux=NULL;
+        if (cab == NULL)
+        {
+            cab = aux;
+            cab->acumulador = 1;
+            aux = NULL;
 
             free(aux);
         }
-
+        return 0;
     }
     else
     {
@@ -52,19 +53,18 @@ void registrar()
         cout << "ingrese su primer nombre: ";
         cin >> aux->Nombre;
 
-        cout<<"ingrese su primer apellido: ";
-        cin>> aux->Apellido;
+        cout << "ingrese su primer apellido: ";
+        cin >> aux->Apellido;
 
         aux->sig = NULL;
         aux2 = cab;
-        
+
         while (aux2->sig != NULL)
         {
             aux2 = aux2->sig;
         }
 
-
-        aux->acumulador = aux2->acumulador+1;
+        aux->acumulador = aux2->acumulador + 1;
         aux2->sig = aux;
         aux = NULL;
         aux2 = aux;
@@ -72,31 +72,31 @@ void registrar()
         free(aux);
         free(aux2);
     }
+    return 0;
 }
 
-void mostrar()
+int mostrar()
 {
-
-    //El orden de llegada inicia desde 0, y no desde 1.
-
     for (aux = cab; aux != NULL; aux = aux->sig)
     {
-        cout<<"Orden de atencion: "<<aux->acumulador<<endl;
-        cout << "Nombre: " << aux->Nombre <<" "<<aux->Apellido<< endl;;
+        cout << "Orden de atencion: " << aux->acumulador + 1 << endl;
+        cout << "Nombre: " << aux->Nombre << " " << aux->Apellido << endl;
         cout << "Numero de Cedula: " << aux->Cedula << endl;
         cout << "" << endl;
     }
+
+    return 0;
 }
 
 int main()
 {
-    int opcion = 0; 
+    int opcion = 0;
 
     do // menu de accion
     {
         cout << "Programa de Registro de Pacientes" << endl;
-        cout<<"Por favor, elija una opcion"<<endl;
-        cout<<""<<endl;
+        cout << "Por favor, elija una opcion" << endl;
+        cout << "" << endl;
 
         cout << "1. Registrar Paciente" << endl;
         cout << "2. Ver Lista de Pacientes" << endl;
@@ -111,6 +111,7 @@ int main()
             break;
 
         case 2:
+            cout << "" << endl;
             cout << "personas registradas: " << endl;
             cout << "" << endl;
             mostrar();
@@ -126,4 +127,5 @@ int main()
         }
     } while (opcion != 3);
 
+    return 0;
 }
